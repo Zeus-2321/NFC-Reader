30a9e3163cc71a272b79b000caf2e7969c7ab1fa
Zeus-2321 <devpratap2321@gmail.com>
2024-04-10 16:06:42 +0530

    Deploy error fixed

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-// import Scanner from './Components/Scanner';
+import Scanner from './Components/Scanner';
 
 function App() {
   return (
     <>
     <h1>Noob</h1>
-    {/* <Scanner></Scanner> */}
+    <Scanner></Scanner>
     </>
   );
 }
